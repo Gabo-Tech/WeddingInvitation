a09522157376c18ee413d1e74592d7ad56251cc1
Gabriel Clemente <clementeaquipresente@yahoo.com>
2023-04-14 17:22:02 +0200

    coun down translation

--- public/js/simplyCountdown.js
+++ public/js/simplyCountdown.js
@@ -115,32 +115,32 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2023,
-                month: 5,
-                day: 12,
-                hours: 0,
-                minutes: 0,
-                seconds: 0,
-                words: {
-                    days: 'día',
-                    hours: 'hora',
-                    minutes: 'minuto',
-                    seconds: 'segundo',
-                    pluralLetter: 's'
-                },
-                plural: true,
-                inline: false,
-                enableUtc: true,
-                onEnd: function () {
-                    return;
-                },
-                refresh: 1000,
-                inlineClass: 'simply-countdown-inline',
-                sectionClass: 'simply-section',
-                amountClass: 'simply-amount',
-                wordClass: 'simply-word',
-                zeroPad: false
-            }, args),
+            year: 2023,
+            month: 5,
+            day: 12,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            words: {
+                days: 'D',
+                hours: 'H',
+                minutes: 'M',
+                seconds: 'S',
+                pluralLetter: ''
+            },
+            plural: true,
+            inline: false,
+            enableUtc: true,
+            onEnd: function () {
+                return;
+            },
+            refresh: 1000,
+            inlineClass: 'simply-countdown-inline',
+            sectionClass: 'simply-section',
+            amountClass: 'simply-amount',
+            wordClass: 'simply-word',
+            zeroPad: false
+        }, args),
             interval,
             targetDate,
             targetTmpDate,
